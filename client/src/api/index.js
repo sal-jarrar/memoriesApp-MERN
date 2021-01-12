494ef8a40de42fe23cd0e402bacee69ec0997349
sultan <sultanjarrar89@gmail.com>
2021-01-12 02:40:57 -0800

    make it better2

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = 'http://localhost:5000/posts'
+const url = 'https://sultanmemoriesapp.herokuapp.com/posts'
 
 export const fetchPost = () => axios.get(url)
 export const createPost = (post) => axios.post(url, post)
