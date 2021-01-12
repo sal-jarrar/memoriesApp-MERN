e359525eb84436ce76143054718c0d19457707a3
sultan <sultanjarrar89@gmail.com>
2021-01-12 01:46:22 -0800

    deploying

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import path from 'path'
+import morgan from 'morgan'
 import connectDB from './config/db.js'
 import postRoute from './routs/posts.js'
 
@@ -10,14 +12,29 @@ dotenv.config()
 connectDB()
 
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 app.use(cors())
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb' }))
 app.use('/posts', postRoute)
 
-app.get('/', (req, res) => {
-  res.send('API is running....')
-})
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`server running in ${process.env.NODE_ENV} on port ${PORT}`)
