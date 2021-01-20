ed945ceb179a53a319b1614229e7615cb214c798
sultan <sultanjarrar89@gmail.com>
2021-01-20 00:52:10 -0800

    updating auth

--- server/middleware/Auth.js
+++ server/middleware/Auth.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken'
+import User from '../models/user.js'
 
 const auth = async (req, res, next) => {
   let token
@@ -10,7 +11,7 @@ const auth = async (req, res, next) => {
     try {
       token = req.headers.authorization.split(' ')[1]
 
-      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+      const decoded = jwt.verify(token, process.env.SECRET)
 
       req.user = await User.findById(decoded.id).select('-password')
 
