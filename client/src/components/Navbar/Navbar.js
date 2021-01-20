80209173886542cec196bf60dc38e803d87d8722
sultan <sultanjarrar89@gmail.com>
2021-01-20 00:13:45 -0800

    deploy2

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { AppBar, Typography, Toolbar, Avatar, Button } from '@material-ui/core'
 import { Link, useHistory, useLocation } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
+import decode from 'jwt-decode'
 
 import memories from '../../images/memories.png'
 import UserTypes from '../../redux/types/UserTypes'
